32cb31b220dad86161e1dfebad9b307335fa5eb6
DmitryDNS <moskvin55ru@yandex.ru>
2023-04-07 00:19:29 +0600

    commit 3 by win7

--- testconsoleappfortestgit/Program.cs
+++ testconsoleappfortestgit/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!"); 
 Console.WriteLine("Hello, World! step 2 by coomit 2 by win10");
-
+Console.WriteLine("Hello, World! step 3 by coomit 3 by win7");
 
